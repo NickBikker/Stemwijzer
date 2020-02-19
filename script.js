15dcc306dcd7bdadc7de11f71285d73c4b8b19c0
Color <nickbikkerprive@gmail.com>
2020-02-19 14:29:36 +0100

    Added start button
    *start button added
    *some button styling

--- script.js
+++ script.js
@@ -5,36 +5,52 @@ var mening = document.getElementById('mening')
 var vraag = 0;
 var votes = [];
 
+oneensbtn.style.display = 'none';
+geenbtn.style.display = 'none';
+eensbtn.style.display = 'none';
+backbtn.style.display = 'none';
+partijbtn.style.display = 'none';
 
 
 
+start = () => {
+    startbtn.style.display = 'none';
+    oneensbtn.style.display = 'inline-block';
+    geenbtn.style.display = 'inline-block';
+    eensbtn.style.display = 'inline-block';
+    backbtn.style.display = 'inline-block';
+    partijbtn.style.display = 'inline-block';
+    laadvraag(vraag);
+}
 
-function laadvraag(question){
+
+
+function laadvraag(question) {
     titel.innerText = subjects[question]["title"];
     mening.innerText = subjects[question]["statement"];
 
 }
 
-function stem(vote){
+function stem(vote) {
     votes[vraag] = vote;
     vraag++;
     laadvraag(vraag);
 
 }
 
-function terug(){
-    if(vraag>0){
+function terug() {
+    if (vraag > 0) {
         vraag--;
         laadvraag(vraag);
     }
 }
 
 
-function partijmening(){
-   
+function partijmening() {
+
     partijbox = document.getElementById('partijen');
 
-    subjects[vraag]['parties'].forEach(function(value, key){
+    subjects[vraag]['parties'].forEach(function (value, key) {
         PartyName = document.createElement('h1');
         PartyPosition = document.createElement('p');
         PartyOpinion = document.createElement('p');
@@ -53,4 +69,3 @@ function partijmening(){
 
 
 
-laadvraag(vraag);
