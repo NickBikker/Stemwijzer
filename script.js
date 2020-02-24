2053808aaa8e18eab7d234f699163647b742924d
nick <nickbikkerprive@gmail.com>
2020-02-25 00:59:05 +0100

    Added finished button
    *Added finished button at last question
    *Adding weighted questions WIP

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ var mening = document.getElementById('mening')
 
 var vraag = 0;
 var votes = [];
+var weightedquestions = []
 
 oneensbtn.style.display = 'none';
 geenbtn.style.display = 'none';
 eensbtn.style.display = 'none';
 backbtn.style.display = 'none';
 partijbtn.style.display = 'none';
+finishbtn.style.display = 'none';
 
 
 
@@ -26,8 +28,17 @@ start = () => {
 
 
 function laadvraag(question) {
-    titel.innerText = subjects[question]["title"];
-    mening.innerText = subjects[question]["statement"];
+    if (question < 29) {
+        titel.innerText = subjects[question]["title"];
+        mening.innerText = subjects[question]["statement"];
+    } else {
+        oneensbtn.style.display = 'none';
+        geenbtn.style.display = 'none';
+        eensbtn.style.display = 'none';
+        finishbtn.style.display = 'inline-block';
+
+    }
+
 
 }
 
@@ -40,6 +51,9 @@ function stem(vote) {
 
 function terug() {
     if (vraag > 0) {
+        oneensbtn.style.display = 'inline-block';
+        geenbtn.style.display = 'inline-block';
+        eensbtn.style.display = 'inline-block';
         vraag--;
         laadvraag(vraag);
     }
@@ -51,9 +65,9 @@ function partijmening() {
     partijbox = document.getElementById('partijen');
 
     subjects[vraag]['parties'].forEach(function (value, key) {
-        PartyName = document.createElement('h1');
-        PartyPosition = document.createElement('p');
-        PartyOpinion = document.createElement('p');
+        var PartyName = document.createElement('h1');
+        var PartyPosition = document.createElement('p');
+        var PartyOpinion = document.createElement('p');
         PartyName.innerText = "Partij: " + value['name'];
         PartyPosition.innerText = "Positie: " + value['position'];
         PartyOpinion.innerText = value['opinion'];
@@ -67,5 +81,33 @@ function partijmening() {
 
 }
 
+finish = () => {
+    var vraagid = 0
+    oneensbtn.style.display = 'none';
+    geenbtn.style.display = 'none';
+    eensbtn.style.display = 'none';
+    backbtn.style.display = 'none';
+    partijbtn.style.display = 'none';
+    finishbtn.style.display = 'none';
+    titel.style.display = 'none';
+    mening.style.display = 'none';
+    subjects.forEach(function(value){
+        var vraagtitel = document.createElement('p');
+        var vraagtitel2 = document.createElement('a');
+        vraagtitel.setAttribute('id', vraagid);
+        vraagtitel.innerText = value['title'];
+        vraagtitel2.onclick = weighted;
+        maindiv.appendChild(vraagtitel);
+        vraagtitel.appendChild(vraagtitel2);
+        vraagid++;
+    })
+
+}
+ 
+
+function weighted(vraagid){
+console.log(vraagid);
+
+}
 
 
